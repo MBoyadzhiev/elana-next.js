e1b931840a54223d4bb039b0957495a2cd216d4c
Martin Boyadzhiev <martin.boyadzhiev@clouddo.eu>
2025-04-23 17:39:55 +0300

    Fixed client-server error

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,3 @@
-import { parse } from "path";
-import Pagination from "./components/Pagination";
-
-export default function Home({
-  searchParams,
-}: {
-  searchParams: { page: string };
-}) {
-  return (
-    <Pagination
-      itemCount={100}
-      pageSize={10}
-      currentPage={parseInt(searchParams.page)}
-    />
-  );
+export default function Home() {
+  return <div> Hello</div>;
 }
