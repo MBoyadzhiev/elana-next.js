d6d92770116eed5555099275f2727d6c26d1af4f
Aleksandar Stankov <aleksandar.stankov@clouddo.eu>
2025-04-23 16:39:11 +0300

    Added user login with credentials and endpoint for registering new users

--- auth.ts
+++ auth.ts
@@ -1,14 +1,35 @@
 import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
-import {PrismaAdapter} from "@auth/prisma-adapter"
 import { prisma } from "./lib/prisma"
+import GoogleProvider from "next-auth/providers/google"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from 'bcrypt';
  
 export const handler = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [GoogleProvider({
     clientId: process.env.GOOGLE_CLIENT_ID!,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-  })],
+  }),
+  CredentialsProvider({
+    name: 'Credentials',
+    credentials: {
+        email:{label:'Email',type:'email',placeholder:'email'},
+        password:{label:'Password',type:'password',placeholder:'password'},
+    },
+    async authorize(credentials)
+    {
+        if(!credentials?.email || !credentials?.password)
+            return null;
+
+        const user = await prisma.user.findUnique({where:{email:credentials.email}});
+
+        if (!user) return null;
+
+       const passwordMatch = await bcrypt.compare(credentials.password,user.hashedPassword!);
+
+       return passwordMatch ? user : null;
+}})],
   session:
   {
     strategy: "jwt"
