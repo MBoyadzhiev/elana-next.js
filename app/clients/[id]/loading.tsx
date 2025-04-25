ddde2349e5fbff4a44f179f607be123707a00fdd
Martin Boyadzhiev <martin.boyadzhiev@clouddo.eu>
2025-04-25 15:36:44 +0300

    fixed not working errors

--- app/clients/[id]/loading.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const LoadingClientsLogPage = () => {
-  return <div>Loading ...</div>;
-};
-
-export default LoadingClientsLogPage;
--- app/components/FormField.tsx
+++ app/components/FormField.tsx
@@ -1,12 +1,58 @@
+// import * as Label from "@radix-ui/react-label";
+// import React from "react";
+
+// interface FormFieldProps {
+//   id: string;
+//   label: string;
+//   type?: "text" | "email";
+//   placeholder?: string;
+//   register?: any;
+//   errorMessage?: string;
+// }
+
+// const FormField: React.FC<FormFieldProps> = ({
+//   id,
+//   label,
+//   type = "text",
+//   placeholder,
+//   register,
+//   errorMessage,
+// }) => {
+//   return (
+//     <div className="space-y-2">
+//       <Label.Root
+//         htmlFor={id}
+//         className="block text-sm font-medium text-gray-700"
+//       >
+//         {label}
+//       </Label.Root>
+//       <input
+//         id={id}
+//         name={id}
+//         type={type}
+//         placeholder={placeholder}
+//         className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+//         {...register(id)}
+//       />
+//       {errorMessage && (
+//         <p className="text-sm text-red-600 mt-1">{errorMessage}</p>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default FormField;
+
 import * as Label from "@radix-ui/react-label";
 import React from "react";
+import { UseFormRegister } from "react-hook-form";
 
 interface FormFieldProps {
   id: string;
   label: string;
   type?: "text" | "email";
   placeholder?: string;
-  register?: any;
+  register?: UseFormRegister<any>;
   errorMessage?: string;
 }
 
@@ -32,7 +78,7 @@ const FormField: React.FC<FormFieldProps> = ({
         type={type}
         placeholder={placeholder}
         className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-        {...register(id)}
+        {...register?.(id)}
       />
       {errorMessage && (
         <p className="text-sm text-red-600 mt-1">{errorMessage}</p>
